7378534b38cfd59030fabe002a9e0c8d44294415
Ryan Slack <ryan@evine.ca>
2016-02-23 23:09:01 -0700

    block-map parser

--- src/main/scala/fastparse/Yaml.scala
+++ src/main/scala/fastparse/Yaml.scala
@@ -1,5 +1,109 @@
 package fastparse.yaml
 
+/*
+
+=Design Goals=
+- easy to learn, ideal for one time use
+ - no special syntax
+ - use Scala types, not rather then YAML
+ - hide/ignore YAML "features"
+- simple for the default case
+ - order of list and map entries does not matter
+ - tuple for a short list of fixed size
+- mostly used for short one time parse
+ - performance is a lower priority
+
+
+=Maps=
+Maps form the backbone as parser only branches on map keys.
+
+Map(
+(
+"foo" -> string,
+"bar" -> mapOf(string->string).map(m:Map[String,String]=>new Foo(m)),
+"annotations" -> collect("(.*)".r -> annotation) //Map[String,Annotation]
+collect("\\?.*)".r -> optional)
+
+
+).map(m:((String,String),(String,Foo)),(Map[String,Annotation])=> Obj(m._1._2,m._2._2)
+.map(match{ case (("foo",foo:String),("bar",bar:Foo)) => Obj(foo,bar) })
+
+def collect[K,V](pair:(K,V)):Map[K,V]
+
+
+ObjectMap("foo","bar")(string, listOf(..))
+.map(Obj.tupled)
+
+ObjectMap("foo","bar")(string, listOf(..))
+
+
+class ObjectMap[F1,F2] (
+((String,F1),(String,F2))
+){
+def map[T](f:(F1,F2)=>T
+}
+((String,F1),(String,F2)) => (F1,F2)
+((String,F1),(String,F2),(String,F3)) => (F1,F2,F3)
+
+
+class Node[T](t:T){
+ def map[V](f:T=>V):V = f(t)
+}
+
+def Map[T](m:T):Node[T] = new Node(m)
+
+=Lists=
+listOf(n:Node) => list(n.rep())
+
+
+=Scalar patterns=
+string(prefix:String="",suffix:String="")
+""
+"".r
+String => Boolean
+Parser
+
+=flatMap=
+
+
+optional()
+either() can not mix types (simple)
+anyof() can mix types (complex)
+
+
+=======
+just produce case classes (list,map,scalar) for second pass parsing
+parse() calls match
+
+Yaml(
+"title"->string,
+"documentation" -> listOf(map...)
+"annotations"-> map(
+(string -> map)(match { case (k:String,v:Map) if k.startsWith("/") => (k,v)})
+)()
+
+) match {
+case m:Map => // val Raml(m.get("title"),m.get("descirption"),m.get("annotations").map(
+}
+
+val resource = map("type" -> , string -> httpmethod)
+
+object(""-> , string -> fn )
+
+
+
+*/
+
+/*
+
+Parser {
+call parseReq
+on success
+
+}
+
+ */
+
 object Yaml {
   def Map(pairs:(Element[K], Element[T])*):MapElement[Map[K,T]]
 }
@@ -32,6 +136,7 @@ abstract class Parsers[T] {
   def block_node(y:Y)
   def block_indented(y:Y)
   def e_scalar = Fail
+  def yaml_content(y:Y)
   def json_content(y:Y)
 
   // [159]   ns-flow-yaml-node(n,c)             ::= c-ns-alias-node | ns-flow-yaml-content(n,c) | ( c-ns-properties(n,c) ( ( s-separate(n,c) ns-flow-yaml-content(n,c) ) | e-scalar ) )
@@ -43,46 +148,63 @@ abstract class Parsers[T] {
 
   // [160]   c-flow-json-node(n,c)              ::= ( c-ns-properties(n,c) s-separate(n,c) )? c-flow-json-content(n,c)
   private def json_node(y:Y) = ( y.properties ~ y.separate )? json-content(y)
+
+  // [161]   ns-flow-node(n,c)                  ::= c-ns-alias-node | ns-flow-content(n,c) | ( c-ns-properties(n,c) ( ( s-separate(n,c) ns-flow-content(n,c) ) | e-scalar ) )
+  //  [158]   ns-flow-content(n,c)               ::= ns-flow-yaml-content(n,c) | c-flow-json-content(n,c)
+  private def flow_node(y:Y) = {
+    val flow_content = yaml_content(y) | P(json_content(y))
+    //alias_node ~/ Fail |
+    flow_content | ( y.properties ~ P( ( y.separate ~ flow_content ) | e_scalar ) )
+  }
 }
 
 class MapParsers[K,V] (val pairs:Seq[(Element[K], Element[V])]) extends Parsers[(K,V)]{
     //[196]   s-l+block-node(n,c)                ::= s-l+block-in-block(n,c) | s-l+flow-in-block(n)
     // [198]   s-l+block-in-block(n,c)            ::= s-l+block-scalar(n,c) | s-l+block-collection(n,c)
     //  [200]   s-l+block-collection(n,c)          ::= ( s-separate(n+1,c) c-ns-properties(n+1,c) )? s-l-comments ( l+block-sequence(seq-spaces(n,c)) | l+block-mapping(n) )
+    // [197]   s-l+flow-in-block(n)               ::= s-separate(n+1,flow-out) ns-flow-node(n+1,flow-out) s-l-comments
   def block_node(y:Y) = {
     val y1 = Y(y.indent+1,y.context)
     val seq_indent = if(y.context == BlockOut) y.indent-1 else y.indent
 
-    //not block-scalar
-    !( y1.separate ~ (y1.properties ~ y1.separate).? ~ CharIn("|<") ) ~
-      ( y1.separate ~ y1.properties ).? ~ comments ~
-    y.indent_more.flatMap((i:Int) => block_mapping(Y(i,y.context)))
+    
+    val not_block_scalar = !( y1.separate ~ (y1.properties ~ y1.separate).? ~ CharIn("|<") ) // cache me
+    val not_block_seq_entry =  !(y.s_indent ~ " -" ~ CharIn(" \t\n\l")) // cache me
+    val block_in_block = 
+      not_block_scalar ~
+      ( y1.separate ~ y1.properties ).? ~ comments ~ 
+      not_block_seq_entry ~
+      y.indent_more.flatMap((i:Int) => block_mapping(Y(i,y.context)))
+
+    val y1FO = Y(y.indent+1,FlowOut)
+    val flow_in_block = y1FO.separate ~ flow_node(y1FO) ~ comments
   }
-  // compact_mapping works out to the same as block_mapping
+  
   def block_indented(y:Y) = indent_any.flatMap((i:Int) => block_mapping(Y(y.indent+1+i,y.context))) | block_node(y) //| ( e_node s-l-comments )
 
-  private val not_block_seq_entry =  !("-" ~ CharIn(" \t\n\l"))
-  //no-cache
+
+
   private def block_collection(y:Y) = y.indent_more.flatMap(n=>block_mapping(Y(n,y.context)))
 
+  // [187]   l+block-mapping(n)                 ::= ( s-indent(n+m) ns-l-block-map-entry(n+m) )+ /* For some fixed auto-detected m > 0 */
   //c-l-block-seq-entry(n)             ::= “-” /* Not followed by an ns-char */
-  private def block_mapping(y:Y) = !("-" ~ CharIn(" \t\n\l")) ~ block_map_entry(y).rep(min=1,delim=y.indent)
+  private def block_mapping(y:Y) = not_block_seq_entry ~ block_map_entry(y).rep(min=1,sep=y.s_indent)
 
   private def block_map_entry(y:Y) = (
-    "?" ~/ flatMap(block_map_explicit_entry(y)),
+    
 
   )
 
   // [189]   c-l-block-map-explicit-entry(n)    ::= c-l-block-map-explicit-key(n) ( l-block-map-explicit-value(n) | e-node )
   //  [190]   c-l-block-map-explicit-key(n)      ::= “?” s-l+block-indented(n,block-out)
   //  [191]   l-block-map-explicit-value(n)      ::= s-indent(n) “:” s-l+block-indented(n,block-out)
-  private def block_map_explicit_entry(y:Y) = {
+  private def block_map_explicit_entry(y:Y) = "?" ~/ P(
     val yBlockOut = Y(y.indent,BlockOut)
-    Either(
+    Either (
       for((key,value) <- pairs) yield
         (key.block_indented(yBlockOut) ~ ":" ~/ value.block_indented(yBlockOut))
     )
-  }
+  )
 
   // [192]   ns-l-block-map-implicit-entry(n)   ::= ( ns-s-block-map-implicit-key | e-node ) c-l-block-map-implicit-value(n)
   //  [193]   ns-s-block-map-implicit-key        ::= c-s-implicit-json-key(block-key) | ns-s-implicit-yaml-key(block-key)
@@ -104,7 +226,47 @@ class MapParsers[K,V] (val pairs:Seq[(Element[K], Element[V])]) extends Parsers[
   // [155]   c-s-implicit-json-key(c)           ::= c-flow-json-node(n/a,c) s-separate-in-line? /* At most 1024 characters altogether */
   //private def implicit_json_key(elem:Element[T], y:Y) = elem.json_node(y) ~ y.separate_in_line.?
 
+  // [161]   ns-flow-node(n,c)                  ::= c-ns-alias-node | ns-flow-content(n,c) | ( c-ns-properties(n,c) ( ( s-separate(n,c) ns-flow-content(n,c) ) | e-scalar ) )
+  //  [158]   ns-flow-content(n,c)               ::= ns-flow-yaml-content(n,c) | c-flow-json-content(n,c)
+  private def flow_node(y:Y) = {
+    // only json-content can be a map, and yaml-content can not start with "{", so no need for negative lookahead
+    val flow_content = json_content(y)
+    //alias_node ~/ Fail |
+    flow_content | ( y.properties ~ P( ( y.separate ~ flow_content ) | e_scalar ) )
+  }
 
+  def yaml_content(y:Y) = Fail
+
+  // [157]   c-flow-json-content(n,c)           ::= c-flow-sequence(n,c) | c-flow-mapping(n,c) | c-single-quoted(n,c) | c-double-quoted(n,c)
+  //  [140]   c-flow-mapping(n,c)                ::= “{” s-separate(n,c)? ns-s-flow-map-entries(n,in-flow(c))? “}”
+  //   [141]   ns-s-flow-map-entries(n,c)         ::= ns-flow-map-entry(n,c) s-separate(n,c)? ( “,” s-separate(n,c)? ns-s-flow-map-entries(n,c)? )?
+  //    [142]   ns-flow-map-entry(n,c)             ::= ( “?” s-separate(n,c) ns-flow-map-explicit-entry(n,c) ) | ns-flow-map-implicit-entry(n,c)
+  def json_content(y:Y) = "{" ~/ y.separate.? ~ P(flow_map_entries(Y(y.indent,y.in_flow))).?  ~ "}"
+  private def flow_map_entries(y:Y) = {
+    val flow_map_implicit_entry = flow_map_implicit_entry(y)
+    (( "?" ~ y.separate ~/ flow_map_implicit_entry //| (e_node.! ~ e_node.!) TODO: support for matching on empty key and empty value
+                                                  ) | flow_map_implicit_entry)
+      .rep(min=1,sep=(y.separate.? ~ "," ~ y.separate.?))
+  }
+
+  // [143]   ns-flow-map-explicit-entry(n,c)    ::= ns-flow-map-implicit-entry(n,c) | ( e-node /* Key */ e-node /* Value */ )
+  // [144]   ns-flow-map-implicit-entry(n,c)    ::= ns-flow-map-yaml-key-entry(n,c) | c-ns-flow-map-empty-key-entry(n,c) | c-ns-flow-map-json-key-entry(n,c)
+  //  [145]   ns-flow-map-yaml-key-entry(n,c)    ::= ns-flow-yaml-node(n,c) ( ( s-separate(n,c)? c-ns-flow-map-separate-value(n,c) ) | e-node )
+  //  [148]   c-ns-flow-map-json-key-entry(n,c)  ::= c-flow-json-node(n,c) ( ( s-separate(n,c)? c-ns-flow-map-adjacent-value(n,c) ) | e-node )
+  private def flow_map_implicit_entry(y:Y) = {
+    Either(
+      for((key, value) <- pairs) yield {
+        // [147]   c-ns-flow-map-separate-value(n,c)  ::= ":" /* Not followed by an ns-plain-safe(c) */ ( ( s-separate(n,c) ns-flow-node(n,c) ) | e-node /* Value */ )
+        val flow_map_separate_value = ":" ~ !y.plain_safe ~/ ( ( y.separate ~ value.flow_node(y) ) | value.e_node )
+        //ns_flow_map_yaml_key_entry
+        (key.yaml_node(y) ~/ ( ( y.separate.? ~ flow_map_separate_value ) | value.e_node )) |
+        //c-ns-flow-map-empty-key-entry(n,c)
+        P(key.e_node ~ flow_map_separate_value ) |
+        // c-ns-flow-map-json-key-entry(n,c)
+        P(key.json_node ~/ ( ( s_separate.? ~ ":" ~/ ( ( y.separate.? ~ value.flow_node(y) ) | value.e_node ) ) | value.e_node ))
+      }
+    )
+  }
 }
 
 class ListParsers[T]{
